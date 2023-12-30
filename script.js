73b4ded0fd190a33889d42b62fae757b3cc45e74
Anurag-996 <112651151+Anurag-996@users.noreply.github.com>
2023-12-30 22:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+    input.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <= 9) {
+            inputs[index].value = '';
+            setTimeout(() => inputs[index + 1]?.focus(), 10);
+        } else if(e.key === 'Backspace') {
+            inputs[index - 1]?.focus();
+        }
+    });
+});
